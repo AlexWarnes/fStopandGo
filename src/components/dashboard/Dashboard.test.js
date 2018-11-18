d33d87b39564d15878b3f5f4120aaba7dd90c1a0
Alex Warnes <alex.s.warnes@gmail.com>
2018-11-17 22:50:51 -0500

    initial test

---
+++ src/components/dashboard/Dashboard.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import { Dashboard } from './Dashboard';
+
+describe('<Dashboard />', () => {
+  it('Should render without crashing', () => {
+    const dispatch = jest.fn();
+
+    shallow(<Dashboard dispatch={dispatch}/>);
+  })
+})
