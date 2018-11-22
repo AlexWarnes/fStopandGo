1e1c4ad542929bc38e3fb5a14cb2991e773c4a42
Alex Warnes <alex.s.warnes@gmail.com>
2018-11-22 17:00:55 -0500

    added display and clear validation error actions

--- src/store/actions/uiActions.js
+++ src/store/actions/uiActions.js
@@ -16,3 +16,33 @@ export const toggleError = (errorIsDisplayed) => ({
   type: TOGGLE_ERROR,
   errorIsDisplayed
 })
+
+export const normalizeResponseErrors = res => {
+  console.log('RUNNING NORMALIZING ERRORS')
+  if (!res.ok) {
+      if (
+          res.headers.has('content-type') &&
+          res.headers.get('content-type').startsWith('application/json')
+      ) {
+        console.log('BLOCK ONE')
+          return res.json().then(err => Promise.reject(err));
+      }
+      return Promise.reject({
+          code: res.status,
+          message: res.statusText
+      });
+  }
+  return res;
+};
+
+export const DISPLAY_VALIDATION_ERROR = 'DISPLAY_VALIDATION_ERROR';
+export const displayValidationError = (message) => ({
+  type: DISPLAY_VALIDATION_ERROR,
+  message
+})
+
+export const CLEAR_VALIDATION_ERROR = 'CLEAR_VALIDATION_ERROR';
+export const clearValidationError = () => ({
+  type: CLEAR_VALIDATION_ERROR
+  
+})
--- src/store/reducers/uiReducer.js
+++ src/store/reducers/uiReducer.js
@@ -1,9 +1,11 @@
-import { TOGGLE_NAV_DRAWER, TOGGLE_WARNING, TOGGLE_ERROR } from "../actions/uiActions";
+import { TOGGLE_NAV_DRAWER, TOGGLE_WARNING, TOGGLE_ERROR, DISPLAY_VALIDATION_ERROR, CLEAR_VALIDATION_ERROR } from "../actions/uiActions";
 
 const initialState = {
     navDrawerIsOpen: false,
     warningIsDisplayed: false,
-    errorIsDisplayed: false
+    errorIsDisplayed: false,
+    isValidationError: false,
+    validationMessage: null
 };
 
 const uiReducer = (state = initialState, action) => {
@@ -22,6 +24,18 @@ const uiReducer = (state = initialState, action) => {
             return Object.assign({}, state, {
                 ...state,
                 errorIsDisplayed: action.errorIsDisplayed
+            });
+        case DISPLAY_VALIDATION_ERROR:
+            return Object.assign({}, state, {
+                ...state,
+                isValidationError: true,
+                validationMessage: action.message        
+            });
+        case CLEAR_VALIDATION_ERROR:
+            return Object.assign({}, state, {
+                ...state,
+                isValidationError: false,
+                validationMessage: null
             });
         default:
             return state;
