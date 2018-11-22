367d786a0785729bb37611a92c3026742123653d
Alex Warnes <alex.s.warnes@gmail.com>
2018-11-22 10:55:35 -0500

    fixed module export

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,1 @@
-// module.exports = {
-//   API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "http://localhost:8080"
-// };
-
 exports.API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080';
