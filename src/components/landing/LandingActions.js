004a0f0efcb6b787174592531bf25914c9de9610
Alex Warnes <alex.s.warnes@gmail.com>
2018-11-29 13:02:44 -0500

    removed arbitrary connect

--- src/components/landing/LandingActions.js
+++ src/components/landing/LandingActions.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
 
 import './LandingActions.css';
 import { demoLogin } from '../../store/actions/authActions';
 import { demoData } from '../../store/actions/photoshootActions';
 
-export const LandingActions = (props) => {   
+export const LandingActions = ({dispatch}) => {   
 
     const launchDemoAccount = () => {
-        props.dispatch(demoLogin());
-        props.dispatch(demoData());
+        dispatch(demoLogin());
+        dispatch(demoData());
     }
 
     return(
         <div className="landing-nav">
             {/* TODO: Link to game */}
             <p className="landing-nav-logo">
-                <i class="material-icons landing-nav-icon">filter_center_focus</i>
+                <i className="material-icons landing-nav-icon">filter_center_focus</i>
                 f/StopandGo
             </p>
             <section className="landing-actions">
@@ -28,4 +27,4 @@ export const LandingActions = (props) => {
     );
 }
 
-export default connect()(LandingActions)
+export default LandingActions;
