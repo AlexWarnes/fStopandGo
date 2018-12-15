12d23371493f17285bacb78ea0a485b2343df444
Alex Warnes <alex.s.warnes@gmail.com>
2018-12-15 17:15:29 -0500

    added About component

---
+++ src/components/dashboard/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Landing from '../landing/Landing';
+
+export const About = () => {
+  return(
+    <div>
+      <Landing />
+    </div>
+  );
+}
+
+export default About;
